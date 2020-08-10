4b56bcfa1a7442aad3b78d7c3520dfe0e9526383
mmkvdev <madhuv299@gmail.com>
2020-08-10 20:32:08 +0530

    Update: Field Code CleanUp

--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -5,6 +5,6 @@ export const validationSchema = Yup.object({
     email: Yup.string()
         .email('Invalid Email Format')
         .required('Required'),
-    channel: Yup.string().required('Required')
-
+    channel: Yup.string().required('Required'),
+    address: Yup.string().required('Address is Required Properly')
 })
