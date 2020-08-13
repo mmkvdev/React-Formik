d6e69134d72f9b572f2461a378bf45b8ea2fc58e
Madhu Kiran Varma <madhuv299@gmail.com>
2020-08-13 12:56:14 +0530

    Update: Load Saved Data

---
+++ src/utils/savedValues.js
@@ -0,0 +1,13 @@
+export const savedValues = {
+    name: 'MMK',
+    email: 'madhuv299@gmail.com',
+    channel: 'MMK',
+    comments: 'Welcome to my World!!',
+    address: '221B Baker Street',
+    social: {
+        facebook: '',
+        twitter: ''
+    },
+    phoneNumbers: ['',''],
+    phNumbers: ['']
+};
